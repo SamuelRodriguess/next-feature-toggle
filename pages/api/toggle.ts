160b94828749883c128c58b7d8b83996c77297ba
samuel.rodrigues <samuel.rodrigues@acct>
2023-06-07 22:06:51 -0300

    chore: about api routes

--- pages/api/toggle.ts
+++ pages/api/toggle.ts
@@ -5,3 +5,5 @@ export default function (req, res) {
       process.env.FEATURE_TREASURY_CHART === "true" ? "treasury_chart" : "",
     ]);
 }
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
